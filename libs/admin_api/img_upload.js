e8b00665305077d9b2a69296dac2e5305960e380
Travis Warlick <twarlick@gmail.com>
2018-02-08 21:11:13 -0500

    Fix image upload (add multer)

--- libs/admin_api/img_upload.js
+++ libs/admin_api/img_upload.js
@@ -5,26 +5,48 @@
 // *	@return {response} - success boolean
 // * ———————————————————————————————————————————————————————— * //
 
+const multer = require('multer')
+
 // * enduro dependencies
-const file_uploader = require(enduro.enduro_path + '/libs/admin_utilities/file_uploader')
-const logger = require(enduro.enduro_path + '/libs/logger')
-const admin_rights = require(enduro.enduro_path + '/libs/admin_utilities/admin_rights')
+const file_uploader = require('../admin_utilities/file_uploader')
+const logger = require('../logger')
+const admin_rights = require('../admin_utilities/admin_rights')
 
-// routed call
-module.exports = function img_upload (req, res, next) {
-	logger.timestamp('Trying to upload a file', 'file_uploading')
+// Array of middleware + endpoint
+module.exports = [
+	// middleware to process upload
+	multer({
+		dest: 'tmp/uploads',
+		fileFilter: (req, file, callback) => {
+			if (/^image\//.test(file.mimetype)) {
+				return callback(null, true)
+			}
+			return callback(null, false)
+		}
+		//TODO enduro config to set the upload file size limit, e.g.:
+		// limits: {
+		// 	fileSize: enduro.config.image_file_size_limit
+		// }
+	}).single('file'),
 
-	if (!admin_rights.can_user_do_that(req.user, 'write')) {
-		console.warn(`Permission denied for ${req.user.username}`)
-		return res.status(403).json({ success: false, message: 'Permission denied' })
+	function img_upload (req, res, next) {
+		logger.timestamp('Trying to upload a file', 'file_uploading')
+
+		if (!admin_rights.can_user_do_that(req.user, 'write')) {
+			console.warn(`Permission denied for ${req.user.username}`)
+			return res.status(403).json({ success: false, message: 'Permission denied' })
+		}
+
+		logger.timestamp('uploading file: ' + req.file.originalname, 'file_uploading')
+
+		file_uploader.upload({
+			name: req.file.originalname,
+			path: req.file.path
+		}).then((image_url) => {
+			res.send({ success: true, image_url: image_url })
+		}, (err) => {
+			if (!err) err = new Error('undefined error in rejection')
+			next(err)
+		})
 	}
-
-	logger.timestamp('uploading file: ' + req.files.file.name, 'file_uploading')
-
-	file_uploader.upload(req.files.file).then((image_url) => {
-		res.send({ success: true, image_url: image_url })
-	}, (err) => {
-		if (!err) err = new Error('undefined error in rejection')
-		next(err)
-	})
-}
+]
