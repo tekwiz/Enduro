4e486a262eda0d71a9f45e77372b59525b6bd7b2
Travis Warlick <twarlick@gmail.com>
2018-03-02 12:16:45 -0500

    Use aws-sdk instead of s3 package

--- libs/remote_tools/filesystems/s3_filesystem.js
+++ libs/remote_tools/filesystems/s3_filesystem.js
@@ -4,9 +4,9 @@
 // * ———————————————————————————————————————————————————————— * //
 const filesystem = function () {}
 
-// * vendor dependencies
-const Promise = require('bluebird')
-const s3 = require('s3')
+const url = require('url')
+const AWS = require('aws-sdk')
+const mime = require('mime-types')
 
 // * enduro dependencies
 const logger = require(enduro.enduro_path + '/libs/logger')
@@ -16,50 +16,51 @@ filesystem.prototype.init = function () {
 }
 
 filesystem.prototype.upload = function (filename, path_to_file) {
-	const self = this
+	const destination_url = this.get_remote_url(filename)
 
-	return new Promise(function (resolve, reject) {
-
-		const destination_url = self.get_remote_url(filename)
-
-		const client = s3.createClient({
-			s3Options: {
-				accessKeyId: enduro.config.variables.S3_KEY,
-				secretAccessKey: enduro.config.variables.S3_SECRET,
-				region: enduro.config.s3.region || 'us-west-1',
-			},
-		})
-
-		const params = {
-			localFile: path_to_file,
-			s3Params: {
-				Bucket: enduro.config.s3.bucket,
-				Key: filename,
-				ACL: 'public-read',
-			},
+	var s3 = new AWS.S3({
+		accessKeyId: enduro.config.variables.S3_KEY,
+		secretAccessKey: enduro.config.variables.S3_SECRET,
+		region: enduro.config.s3.region,
+		params: {
+			Bucket: enduro.config.s3.bucket
 		}
-		const uploader = client.uploadFile(params)
+	})
 
-		uploader.on('error', function (err) {
-			console.error('unable to upload:', err.stack)
+	return new Promise((resolve, reject) => {
+		s3.upload({
+			Key: filename,
+			Body: fs.createReadStream(path_to_file),
+			ContentType: mime.contentType(path.extname(filename)),
+			CacheControl: 'public, max-age=31536000'
+		}, (err, data) => {
+			if (err) {
+				console.error(`uploadiung failed ${filename} ${err.stack}`)
+				return reject(err)
+			}
+			logger.timestamp(`File uploaded successfully: ${destination_url}`)
+			resolve(destination_url)
 		})
-
-		uploader.on('end', function () {
-			logger.timestamp('File uploaded successfully: ' + destination_url)
-			return resolve(destination_url)
-		})
-
 	})
 }
 
 filesystem.prototype.get_remote_url = function (filename, juicebox) {
-	if (enduro.config.s3.cloudfront && !juicebox) {
-		return 'https://' + enduro.config.s3.cloudfront + '/' + filename
-	} else if (enduro.config.s3.region === 'us-east-1') {
-		return 'https://s3.amazonaws.com/' + enduro.config.s3.bucket + '/' + filename
-	} else {
-		return 'https://s3-' + (enduro.config.s3.region || 'us-west-1') + '.amazonaws.com/' + enduro.config.s3.bucket + '/' + filename
+	var remoteUrl = {
+		scheme: 'https',
+		hostname: `s3-${enduro.config.s3.region}.amazonaws.com`,
+		pathname: `/${enduro.config.s3.bucket}/${filename}`
 	}
+
+	if (enduro.config.s3.region === 'us-east-1') {
+		remoteUrl.hostname = 's3.amazonaws.com'
+	}
+
+	if (enduro.config.s3.cloudfront && !juicebox) {
+		remoteUrl.hostname = enduro.config.s3.cloudfront
+		remoteUrl.pathname = `/${filename}`
+	}
+
+	return url.format(remoteUrl)
 }
 
 module.exports = new filesystem()
