5fdf645f55e9b7f9c18b12e97e14666080e52e82
Travis Warlick <twarlick@gmail.com>
2018-03-05 07:37:05 -0500

    Optimize & secure CSS output in production

--- libs/build_tools/sass_handler.js
+++ libs/build_tools/sass_handler.js
@@ -7,6 +7,7 @@
 const sass_handler = function () {}
 
 // * vendor dependencies
+const path = require('path')
 const bulkSass = require('gulp-sass-bulk-import')
 const sass = require('gulp-sass')
 const sourcemaps = require('gulp-sourcemaps')
@@ -25,27 +26,46 @@ sass_handler.prototype.init = function (gulp, browser_sync) {
 
 		logger.timestamp('Sass compiling started', 'enduro_events')
 
-		return gulp.src(enduro.project_path + '/assets/css/*.scss')
+		var stream = gulp.src(enduro.project_path + '/assets/css/*.scss')
 			.pipe(bulkSass())
-			.pipe(sourcemaps.init())
-			.pipe(sass())
 			.on('error', function (err) {
 				logger.err_blockStart('Sass error')
 				logger.err(err.message)
 				logger.err_blockEnd()
 				this.emit('end')
 			})
-			.pipe(autoprefixer({
-				browsers: ['last 2 versions'],
-				cascade: false,
-			}))
-			.pipe(sourcemaps.write())
-			.pipe(gulp.dest(enduro.project_path + '/' + enduro.config.build_folder + '/assets/css'))
-			.pipe(browser_sync.stream())
 			.on('end', () => {
 				logger.timestamp('Sass compiling finished', 'enduro_events')
 			})
 
+		if (process.env.NODE_ENV === 'production') {
+			stream = stream.pipe(sass({ outputStyle: 'compressed' }))
+		} else {
+			stream = stream.pipe(sourcemaps.init())
+				.pipe(sass({ outputStyle: 'nested' }))
+		}
+
+		stream = stream
+			.pipe(autoprefixer({
+				browsers: ['last 2 versions'],
+				cascade: false,
+			}))
+
+		if (process.env.NODE_ENV !== 'production') {
+			stream = stream.pipe(sourcemaps.write())
+		}
+
+		let css_dest = path.join(enduro.project_path, enduro.config.build_folder, 'assets', 'css')
+		stream = stream.pipe(gulp.dest(css_dest))
+
+		if (browser_sync) {
+			if (process.env.NODE_ENV === 'production') {
+				console.warn('DANGER: Using browser_sync in production is insecure!')
+			}
+			stream = stream.pipe(browser_sync.stream())
+		}
+
+		return stream
 	})
 
 	return sass_handler_task_name
